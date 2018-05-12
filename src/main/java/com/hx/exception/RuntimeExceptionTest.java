33a88509ffb4c809a97d1d41a9840e9a4d0c299d
huangxing <447994518@qq.com>
2018-05-12 17:31:32 +0800

    update nio

---
+++ src/main/java/com/hx/exception/RuntimeExceptionTest.java
@@ -0,0 +1,21 @@
+package com.hx.exception;
+
+import org.junit.Test;
+
+/**
+ * Created by hx on 18-5-2.
+ */
+public class RuntimeExceptionTest {
+  String a;
+  @Test
+  public void test1(){
+    try {
+      System.out.println(a.toCharArray());
+    }catch (Exception e){
+      e.printStackTrace();
+      System.out.println("err");
+    }
+    System.out.println("continue");
+
+  }
+}
--- src/main/java/com/hx/thread/concurrent/collections/ConcurrentHashMapTest.java
+++ src/main/java/com/hx/thread/concurrent/collections/ConcurrentHashMapTest.java
@@ -2,7 +2,9 @@ package com.hx.thread.concurrent.collections;
 
 import junit.framework.TestCase;
 
+import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -17,6 +19,17 @@ public class ConcurrentHashMapTest extends TestCase {
     t=new Test(1);
     t1=tests.put(t,t);
     t1=tests.putIfAbsent(t,t);
+
+  }
+  public void test2() {
+    Map<String, String > tests = new ConcurrentHashMap<>();
+    tests.put("111","111");
+    tests.put("222","222");
+    Set<String> strings = tests.keySet();
+    Integer i=0;
+    for (String key:strings){
+      tests.put((i++).toString(),"dd");
+    }
 
   }
 

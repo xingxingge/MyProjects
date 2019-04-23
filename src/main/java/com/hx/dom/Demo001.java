1e9dafb6921d7fead5da7f3624bae5bb3a6b1252
huangxing <huangxing@gsafety.com>
2019-04-23 23:30:14 +0800

    commit

---
+++ src/main/java/com/hx/dom/Demo001.java
@@ -0,0 +1,35 @@
+package com.hx.dom;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
+public class Demo001 {
+  public static void main(String[] args) throws Exception {
+    // 1、获取工厂实例
+    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+    // 2、获取dom解析器
+    DocumentBuilder db = dbf.newDocumentBuilder();
+    // 3、解析xml文档，获取document对象(根节点)
+    Document document = db.parse(new File("./data/dom/demo001.xml"));
+
+    NodeList list = document.getElementsByTagName("student");
+
+    for (int i = 0; i < list.getLength(); i++) {
+      // 注意：使用的都是org.w3c.dom包下的
+      Element element = (Element) list.item(i);
+      // element.getElementsByTagName("name").item(0) 是 <name>小明</name>
+      // getFirstChild().getNodeValue() 是获取 <name>小明</name>里面的小明
+      String context = element.getElementsByTagName("name").item(0).getFirstChild().getNodeValue();
+      System.out.println(context);
+      context = element.getElementsByTagName("age").item(0).getFirstChild().getNodeValue();
+      System.out.println(context);
+      context = element.getElementsByTagName("grade").item(0).getFirstChild().getNodeValue();
+      System.out.println(context);
+    }
+  }
+}
---
+++ src/main/java/com/hx/sax/People.java
@@ -0,0 +1,32 @@
+package com.hx.sax;
+public class People {
+  private String id;
+  private String name;
+  private String englishName;
+  private String age;
+  public String getId() {
+    return id;
+  }
+  public void setId(String id) {
+    this.id = id;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getEnglishName() {
+    return englishName;
+  }
+  public void setEnglishName(String englishName) {
+    this.englishName = englishName;
+  }
+  public String getAge() {
+    return age;
+  }
+  public void setAge(String age) {
+    this.age = age;
+  }
+
+}
